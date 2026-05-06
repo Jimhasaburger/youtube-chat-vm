b2fc5837a06f934d6b81fcfcf98d86c35a79a0ff
mint <mint@jimhasaburger.com>
2026-05-06 14:46:38 +0200

    forgot time import

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, jsonify
 import logging
 import uuid
 import virtualbox
+import time
 
 # -------------------- config ---------------
 print("enter your video id:")
